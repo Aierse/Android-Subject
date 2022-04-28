6e8d0c25c6731a63e6fada0bd4a5e9b42c3048ab
Aierse <68111814+Aierse@users.noreply.github.com>
2022-04-28 15:36:14 +0900

    기능 - 에딧텍스트 이벤트

--- Project7_1/app/src/main/java/com/example/project7_1/MainActivity.java
+++ Project7_1/app/src/main/java/com/example/project7_1/MainActivity.java
@@ -3,26 +3,32 @@ package com.example.project7_1;
 import android.graphics.Color;
 import android.os.Bundle;
 
+import com.example.project7_1.databinding.ActivityMainBinding;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.KeyEvent;
 import android.view.MenuInflater;
 import android.view.View;
 
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.example.project7_1.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    CoordinatorLayout coordinatorLayout;
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
@@ -59,12 +65,27 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.main_menu, menu);
+        // 메뉴 아이템 중에서 검색을 위해 정의한 아이템을 뷰 객체로 참조
+        LinearLayout view = (LinearLayout)menu.findItem(R.id.search_button).getActionView();
+
+        EditText edit = view.findViewById(R.id.editText);
+
+        edit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                Toast.makeText(getApplicationContext(), "흐재게이야...", Toast.LENGTH_SHORT).show();
+
+                return false;
+            }
+        });
 
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        coordinatorLayout = findViewById(R.id.coordinator1);
+
         switch (item.getItemId()) {
             case R.id.itemRed:
                 binding.coordinator1.setBackgroundColor(Color.RED);
