c5cd9a4cc7706d004d26d3feee081cbeb86e0693
Aierse <68111814+Aierse@users.noreply.github.com>
2022-05-19 17:04:25 +0900

    리팩토링 - 자료형

--- Project10_1/app/src/main/java/com/example/project10_1/MainActivity.java
+++ Project10_1/app/src/main/java/com/example/project10_1/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         int[] voteCount = new int[9];
         Arrays.fill(voteCount, 0);
 
-        Integer[] imageId = {R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4, R.id.iv5, R.id.iv6, R.id.iv7, R.id.iv8, R.id.iv9};
+        int[] imageId = {R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4, R.id.iv5, R.id.iv6, R.id.iv7, R.id.iv8, R.id.iv9};
         String[] imgName = { "독서하는 소녀", "꽃장식 모자 소녀", "부채를 든 소녀", "이레느깡 단 베르양", "잠자는 소녀", "테라스의 두 자매", "피아노 레슨", "피아노 앞의 소녀들", "해변에서" };
         ImageView[] image = new ImageView[9];
 
