477745d52937b0d4aad56b13f5a001aa8b3b618d
Aierse <68111814+Aierse@users.noreply.github.com>
2022-05-19 16:35:50 +0900

    기능 - 넘겨받은 데이터 적용

--- Project10_1/app/src/main/java/com/example/project10_1/ResultActivity.java
+++ Project10_1/app/src/main/java/com/example/project10_1/ResultActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
     Button btnReturn;
@@ -14,6 +16,25 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         setTitle("투표 결과");
+
+        int voteCount[] = getIntent().getIntArrayExtra("VoteCount");
+        String[] imageName = getIntent().getStringArrayExtra("ImageName");
+
+        RatingBar[] bar = new RatingBar[] {
+                findViewById(R.id.rBar1), findViewById(R.id.rBar2), findViewById(R.id.rBar3),
+                findViewById(R.id.rBar4), findViewById(R.id.rBar5), findViewById(R.id.rBar6),
+                findViewById(R.id.rBar7), findViewById(R.id.rBar8), findViewById(R.id.rBar9)};
+
+        TextView[] tv = new TextView[] {
+                findViewById(R.id.tv1),findViewById(R.id.tv2),findViewById(R.id.tv3),
+                findViewById(R.id.tv4),findViewById(R.id.tv5),findViewById(R.id.tv6),
+                findViewById(R.id.tv7),findViewById(R.id.tv8),findViewById(R.id.tv9),
+        };
+
+        for (int i = 0; i< bar.length; i++) {
+            tv[i].setText(imageName[i]);
+            bar[i].setRating(voteCount[i]);
+        }
 
         btnReturn = findViewById(R.id.btnReturn);
         btnReturn.setOnClickListener(new View.OnClickListener() {
