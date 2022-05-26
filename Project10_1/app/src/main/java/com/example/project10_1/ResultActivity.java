b036437d9fb8278177b927bc3f7f7d15b3700d0a
Aierse <68111814+Aierse@users.noreply.github.com>
2022-05-26 14:34:17 +0900

    리팩토링

--- Project10_1/app/src/main/java/com/example/project10_1/ResultActivity.java
+++ Project10_1/app/src/main/java/com/example/project10_1/ResultActivity.java
@@ -22,9 +22,9 @@ public class ResultActivity extends AppCompatActivity {
         String[] imageName = getIntent().getStringArrayExtra("ImageName");
 
         int[] imageResource = new int[] {
-                R.drawable.pic1,R.drawable.pic2,R.drawable.pic3,
-                R.drawable.pic4,R.drawable.pic5,R.drawable.pic6,
-                R.drawable.pic7,R.drawable.pic8,R.drawable.pic9
+                R.drawable.pic1, R.drawable.pic2, R.drawable.pic3,
+                R.drawable.pic4, R.drawable.pic5, R.drawable.pic6,
+                R.drawable.pic7, R.drawable.pic8, R.drawable.pic9
         };
 
         RatingBar[] bar = new RatingBar[] {
