282ef6e2d02fb1f342fd2285b699a343cb0a2969
Aierse <68111814+Aierse@users.noreply.github.com>
2022-05-26 16:35:55 +0900

    추가 - 커스텀 리스트뷰

--- Project11_1/app/src/main/java/com/example/project11_1/CustomActivity.java
+++ Project11_1/app/src/main/java/com/example/project11_1/CustomActivity.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class DynamicActivity extends AppCompatActivity {
+public class CustomActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dynamic);
-        setTitle("동적리스트뷰");
+        setContentView(R.layout.activity_custom);
+        setTitle("커스텀리스트뷰");
     }
 }
