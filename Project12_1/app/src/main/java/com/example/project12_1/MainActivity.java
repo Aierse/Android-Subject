507d450a26002dcc7602cdbbc203437774fe53cd
Aierse <68111814+Aierse@users.noreply.github.com>
2022-06-09 16:28:47 +0900

    기능 - DB 생성

--- Project12_1/app/src/main/java/com/example/project12_1/MainActivity.java
+++ Project12_1/app/src/main/java/com/example/project12_1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.project12_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SQLiteDatabase db = openOrCreateDatabase("mydatabase.db", Context.MODE_PRIVATE, null);
+        // 테이블 추가
+        String sql = "CREATE TABLE student(id Integer PRIMARY KEY AUTOINCREMENT, name Text, mobile Text);";
+        db.execSQL(sql);
+        // 레코드 추가
+        db.execSQL("INSERT INTO student(name, mobile) VALUES ('kim', '010-1234-5678');");
+        db.execSQL("INSERT INTO student(name, mobile) VALUES ('lee', '010-4455-5678');");
     }
 }
