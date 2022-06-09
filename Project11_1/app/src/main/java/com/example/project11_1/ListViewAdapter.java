69e5017dd6e778969e803eef2596aa6db805203f
Aierse <68111814+Aierse@users.noreply.github.com>
2022-06-09 15:29:13 +0900

    기능 - 리스트 뷰 어댑터

--- Project11_1/app/src/main/java/com/example/project11_1/ListViewAdapter.java
+++ Project11_1/app/src/main/java/com/example/project11_1/ListViewAdapter.java
@@ -1,37 +1,85 @@
 package com.example.project11_1;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ListViewAdapter {
-    class ListViewItem {
-        private Drawable iconDrawable;
-        private String titleStr;
-        private String descStr;
+import java.util.ArrayList;
 
-        public Drawable getIconDrawable() {
-            return iconDrawable;
-        }
+public class ListViewAdapter extends BaseAdapter {
+    private ArrayList<ListViewItem> arrayList = new ArrayList<>();
 
-        public void setIconDrawable(Drawable iconDrawable) {
-            this.iconDrawable = iconDrawable;
-        }
-
-        public String getTitleStr() {
-            return titleStr;
-        }
-
-        public void setTitleStr(String titleStr) {
-            this.titleStr = titleStr;
-        }
-
-
-        public String getDescStr() {
-            return descStr;
-        }
-
-        public void setDescStr(String descStr) {
-            this.descStr = descStr;
-        }
+    @Override
+    public int getCount() {
+        return arrayList.size();
     }
 
+    @Override
+    public Object getItem(int i) {
+        return arrayList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        Context context = viewGroup.getContext();
+
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            view = inflater.inflate(R.layout.listview_item, viewGroup, false);
+
+            ImageView iconImageView = view.findViewById(R.id.imageView1);
+            TextView titleTextView = view.findViewById(R.id.textView1);
+            TextView descTextView = view.findViewById(R.id.textView2);
+
+            ListViewItem item = arrayList.get(i);
+
+            iconImageView.setImageResource(item.getIconDrawable());
+            titleTextView.setText(item.getTitleStr());
+            descTextView.setText(item.getDescStr());
+        }
+
+        return view;
+    }
+}
+
+class ListViewItem {
+    private int iconDrawable;
+    private String titleStr;
+    private String descStr;
+
+    public int getIconDrawable() {
+        return iconDrawable;
+    }
+
+    public void setIconDrawable(int iconDrawable) {
+        this.iconDrawable = iconDrawable;
+    }
+
+    public String getTitleStr() {
+        return titleStr;
+    }
+
+    public void setTitleStr(String titleStr) {
+        this.titleStr = titleStr;
+    }
+
+
+    public String getDescStr() {
+        return descStr;
+    }
+
+    public void setDescStr(String descStr) {
+        this.descStr = descStr;
+    }
 }
