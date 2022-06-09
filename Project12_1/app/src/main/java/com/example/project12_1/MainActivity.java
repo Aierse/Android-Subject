98292210ed0830134e2df4f9b77aeccc63b9cd14
Aierse <68111814+Aierse@users.noreply.github.com>
2022-06-09 17:10:16 +0900

    기능 - SQLiteOpenHelper 상속

--- Project12_1/app/src/main/java/com/example/project12_1/MainActivity.java
+++ Project12_1/app/src/main/java/com/example/project12_1/MainActivity.java
@@ -1,24 +1,43 @@
 package com.example.project12_1;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    myDBHelper myDBHelper;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SQLiteDatabase db = openOrCreateDatabase("mydatabase.db", Context.MODE_PRIVATE, null);
-        // 테이블 추가
-        String sql = "CREATE TABLE student(id Integer PRIMARY KEY AUTOINCREMENT, name Text, mobile Text);";
-        db.execSQL(sql);
-        // 레코드 추가
-        db.execSQL("INSERT INTO student(name, mobile) VALUES ('kim', '010-1234-5678');");
-        db.execSQL("INSERT INTO student(name, mobile) VALUES ('lee', '010-4455-5678');");
+        myDBHelper = new myDBHelper(this);
+        db = myDBHelper.getWritableDatabase();
+
+        //db.close();
+    }
+
+    static class myDBHelper extends SQLiteOpenHelper {
+        //생성자를 통해 DB 생성
+        public myDBHelper(Context context) {
+            super(context, "groupDB.db", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE groupTBL(gName char(20) PRIMARY KEY, gNumber integer);");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS groupTBL");
+            onCreate(db);
+        }
     }
 }
