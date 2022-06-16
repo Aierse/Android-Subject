6bfa039a06b65799a3ded474f4bc714667cb6d59
Aierse <68111814+Aierse@users.noreply.github.com>
2022-06-16 14:56:57 +0900

    기능 - 컴포넌트 초기화

--- Project12_1/app/src/main/java/com/example/project12_1/MainActivity.java
+++ Project12_1/app/src/main/java/com/example/project12_1/MainActivity.java
@@ -1,29 +1,41 @@
 package com.example.project12_1;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     myDBHelper myDBHelper;
     SQLiteDatabase db;
-
+    EditText edtName, edtNumber, edtNameResult, edtNumberResult;
+    Button btnInit, btnInsert, btnSelect, btnUpdate, btnDelete;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myDBHelper = new myDBHelper(this);
-        db = myDBHelper.getWritableDatabase();
+        edtName = findViewById(R.id.edtName);
+        edtNumber = findViewById(R.id.edtNumber);
+        edtNameResult = findViewById(R.id.edtNameResult);
+        edtNumberResult = findViewById(R.id.edtNumberResult);
 
+        btnInit = findViewById(R.id.btnInit);
+        btnInsert = findViewById(R.id.btnInsert);
+        btnSelect = findViewById(R.id.btnSelect);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+
+        //myDBHelper = new myDBHelper(this);
+        //db = myDBHelper.getWritableDatabase();
         //db.close();
     }
 
-    static class myDBHelper extends SQLiteOpenHelper {
+    public class myDBHelper extends SQLiteOpenHelper {
         //생성자를 통해 DB 생성
         public myDBHelper(Context context) {
             super(context, "groupDB.db", null, 1);
