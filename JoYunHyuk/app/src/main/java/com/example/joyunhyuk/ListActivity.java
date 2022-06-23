59058c7fd07c7fb22c36aa60eec2533f1612048c
Aierse <dbsgur2008@gmail.com>
2022-06-23 16:16:04 +0900

    스타일 - 타이틀 이름 수정

--- JoYunHyuk/app/src/main/java/com/example/joyunhyuk/ListActivity.java
+++ JoYunHyuk/app/src/main/java/com/example/joyunhyuk/ListActivity.java
@@ -22,6 +22,7 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        setTitle("기말고사 프로젝트");
 
         myDBHelper = new MyDBHelper(this);
 
