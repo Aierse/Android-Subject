dc1682245a396d1a43cc60ffb1fd1c363e9a9b22
Aierse <dbsgur2008@gmail.com>
2022-06-23 15:46:20 +0900

    기능 - 리스트 뷰 어댑터 연결

--- JoYunHyuk/app/src/main/java/com/example/joyunhyuk/ListActivity.java
+++ JoYunHyuk/app/src/main/java/com/example/joyunhyuk/ListActivity.java
@@ -2,9 +2,16 @@ package com.example.joyunhyuk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -29,6 +36,51 @@ public class ListActivity extends AppCompatActivity {
             password.add(cursor.getString(1));
         }
 
+        ListView listView = findViewById(R.id.itemListView);
+        listView.setAdapter(new ListViewAdapter(email, password));
+    }
+}
 
+class ListViewAdapter extends BaseAdapter {
+    private final ArrayList<String> email;
+    private final ArrayList<String> password;
+
+    public ListViewAdapter(ArrayList<String> email, ArrayList<String> password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public int getCount() {
+        return email.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return email.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        Context context = viewGroup.getContext();
+
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            view = inflater.inflate(R.layout.list_item, viewGroup, false);
+
+            TextView emailTextView = view.findViewById(R.id.itemEmail);
+            TextView passwordTextView = view.findViewById(R.id.itemPassword);
+
+            emailTextView.setText(email.get(i));
+            passwordTextView.setText(password.get(i));
+        }
+
+        return view;
     }
 }
