cd86386b4669a67c2f148b428135d96a1f1843df
Aierse <dbsgur2008@gmail.com>
2022-06-23 14:52:01 +0900

    기능 - 메인 -> 로그인 액티비티 실행

--- JoYunHyuk/app/src/main/java/com/example/joyunhyuk/MainActivity.java
+++ JoYunHyuk/app/src/main/java/com/example/joyunhyuk/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.joyunhyuk;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
@@ -24,7 +27,15 @@ public class MainActivity extends AppCompatActivity {
         insertStudent("9012", "이순신", "010-9999-2222", "lee@gmail.com", "9012!");
         // 내 데이터 추가
         insertStudent("20174434", "조윤혁", "010-3916-0577", "dbsgur2008@naver.com", "1234!");
+
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        mStartForResult.launch(intent);
     }
+
+    ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        if (result.getResultCode() == RESULT_OK) {
+        }
+    });
 
     private void insertStudent(String dno, String name, String tel, String email, String password) {
         SQLiteDatabase db = myDBHelper.getWritableDatabase();
